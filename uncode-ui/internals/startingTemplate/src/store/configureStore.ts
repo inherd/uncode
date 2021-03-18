fbd58bff02d9364d6175e02878e821e73ceda261
Phodal Huang <h@phodal.com>
2021-03-18 08:00:53 +0800

    build: reinit ts code for we

---
+++ uncode-ui/internals/startingTemplate/src/store/configureStore.ts
@@ -0,0 +1,34 @@
+/**
+ * Create the store with dynamic reducers
+ */
+
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { createInjectorsEnhancer } from 'redux-injectors';
+import createSagaMiddleware from 'redux-saga';
+
+import { createReducer } from './reducers';
+
+export function configureAppStore() {
+  const reduxSagaMonitorOptions = {};
+  const sagaMiddleware = createSagaMiddleware(reduxSagaMonitorOptions);
+  const { run: runSaga } = sagaMiddleware;
+
+  // Create the store with saga middleware
+  const middlewares = [sagaMiddleware];
+
+  const enhancers = [
+    createInjectorsEnhancer({
+      createReducer,
+      runSaga,
+    }),
+  ];
+
+  const store = configureStore({
+    reducer: createReducer(),
+    middleware: [...getDefaultMiddleware(), ...middlewares],
+    devTools: process.env.NODE_ENV !== 'production',
+    enhancers,
+  });
+
+  return store;
+}
---
+++ uncode-ui/src/store/configureStore.ts
@@ -0,0 +1,33 @@
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { createInjectorsEnhancer } from 'redux-injectors';
+import createSagaMiddleware from 'redux-saga';
+
+import { createReducer } from './reducers';
+
+export function configureAppStore() {
+  const reduxSagaMonitorOptions = {};
+  const sagaMiddleware = createSagaMiddleware(reduxSagaMonitorOptions);
+  const { run: runSaga } = sagaMiddleware;
+
+  // Create the store with saga middleware
+  const middlewares = [sagaMiddleware];
+
+  const enhancers = [
+    createInjectorsEnhancer({
+      createReducer,
+      runSaga,
+    }),
+  ];
+
+  const store = configureStore({
+    reducer: createReducer(),
+    middleware: [...getDefaultMiddleware(), ...middlewares],
+    devTools:
+      /* istanbul ignore next line */
+      process.env.NODE_ENV !== 'production' ||
+      process.env.PUBLIC_URL.length > 0,
+    enhancers,
+  });
+
+  return store;
+}
