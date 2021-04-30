8432b78799aebb5c9582c9a17a9befa3ba8b7718
Phodal Huang <h@phodal.com>
2021-05-01 00:02:11 +0800

    refactor: align api

--- uncode-ui/src/tauri-bridge.ts
+++ uncode-ui/src/tauri-bridge.ts
@@ -1,36 +1,20 @@
-import * as dialog from 'tauri/api/dialog';
-import { setTitle } from 'tauri/api/window';
-import { invoke, promisified } from 'tauri/api/tauri';
+import { open } from '@tauri-apps/api/dialog';
+import { invoke } from '@tauri-apps/api/tauri';
 
 const TauriBridge = {
   getStory(): Promise<any> {
-    return this.event('getStory', '', '');
+    return invoke('getSotry');
   },
-  openDialog() {
-    dialog.open({ directory: true }).then(result => {
-      invoke({
-        cmd: 'openDirectory',
-        payload: result,
-      });
+
+  openDialog(): Promise<any> {
+    return open({ directory: true }).then(result => {
+      return invoke('openDirectory', { payload: result });
     });
   },
-  title(title: string) {
-    setTitle(title);
-    this.log('title', title);
-  },
-  log(name: string, message: string) {
-    invoke({
-      cmd: 'logOperation',
-      event: name,
-      payload: message,
-    });
-  },
-  event(cmd: string, event: string, payload: string): Promise<any> {
-    return promisified({
-      cmd: cmd,
-      event: event,
-      payload: payload,
-    });
+
+  title(title: string) {},
+  log(name: string, message: string): Promise<any> {
+    return invoke('logOperation', { payload: message });
   },
 };
 
