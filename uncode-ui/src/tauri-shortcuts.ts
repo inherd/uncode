293729aef707b2c6c20b6a04df4e5cadb39566a5
Phodal Huang <h@phodal.com>
2021-04-30 23:02:28 +0800

    refactor: unbind init for reload issue

--- uncode-ui/src/tauri-shortcuts.ts
+++ uncode-ui/src/tauri-shortcuts.ts
@@ -7,6 +7,9 @@ const TauriShortcuts = {
   },
 
   init() {
+    hotkeys.unbind('f5');
+    hotkeys.unbind('ctrl+o, command+o');
+
     hotkeys('f5', function (event, handler) {
       event.preventDefault();
       return false;
