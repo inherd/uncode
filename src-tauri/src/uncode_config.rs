c50ac31f4d535ad519a2014d50ab170f7d44a61e
Phodal Huang <h@phodal.com>
2021-05-01 11:53:56 +0800

    feat: make config to home dir

--- src-tauri/src/uncode_config.rs
+++ src-tauri/src/uncode_config.rs
@@ -1,6 +1,7 @@
 use std::fs::File;
 use std::fs;
 use serde::{Serialize, Deserialize};
+use std::path::PathBuf;
 
 #[derive(Serialize, Deserialize)]
 pub struct UncodeConfig {
@@ -23,7 +24,7 @@ impl UncodeConfig {
     }
 
     let ws = serde_json::to_string_pretty(self.clone()).expect("error");
-    let path = PathBuf::from(self.path.clone()).join(".uncode");
+    let path = UncodeConfig::config_path().expect("lost home issue");
     if !path.exists() {
       let _file = File::create(&path).unwrap();
     }
@@ -37,4 +38,39 @@ impl UncodeConfig {
   pub fn json(&self) -> String {
     serde_json::to_string(self.clone()).expect("error")
   }
+
+  pub fn read_config() -> UncodeConfig {
+    let mut app_state = UncodeConfig::default();
+    let path = UncodeConfig::config_path().expect("lost home issue");
+    let content;
+    match fs::read_to_string(&path) {
+      Ok(str) => {
+        content = str;
+      }
+      Err(_) => {
+        return app_state;
+      }
+    }
+
+    match serde_json::from_str(&content) {
+      Ok(state) => {
+        app_state = state;
+      }
+      Err(_err) => {
+        log::error!("error config: {}", content);
+      }
+    };
+    return app_state;
+  }
+
+  pub fn config_path() -> Option<PathBuf> {
+    let home = dirs::home_dir()?;
+    let base = home.join(".uncode");
+    if !&base.exists() {
+      let _ = fs::create_dir_all(&base);
+    }
+    let config_path = base.join("uncode.json");
+    Some(config_path)
+  }
+
 }
